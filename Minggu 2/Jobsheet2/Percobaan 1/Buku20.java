655689849a9aadd8e34537dd68cb5002866aa4a8
Noklent <noklen350@gmail.com>
2024-02-21 16:34:18 +0700

    Feat : percobaa1_js2_done

---
+++ Minggu 2/Jobsheet2/Percobaan 1/Buku20.java
@@ -0,0 +1,34 @@
+/**
+ * Buku20
+ */
+public class Buku20 {
+
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah Halaman: " + halaman);
+        System.out.println("Sisa Stok: " + stok);
+        System.out.println("Harga: Rp " + harga);
+    }
+
+    void terjual(int jml) {
+        if (stok > 0 && stok >= jml) {
+            stok -= jml;
+        } else {
+            System.out.println("Stok habis");
+            
+        }
+    }
+
+    void restock(int n) {
+
+    }
+
+    void gantiHarga(int hrg) {
+
+    }
+
+}
