fd061a492a38211cc5109cbf62238ed04fe71f8a
Noklent <noklen350@gmail.com>
2024-04-04 10:52:03 +0700

    feat : percobaan 3 js 6 done

--- Minggu 7/Percobaan3/MergeSorting20.java
+++ Minggu 7/Percobaan3/MergeSorting20.java
@@ -1,5 +1,54 @@
 package Percobaan3;
 
 public class MergeSorting20 {
-    
+
+    // mehot mergeSort
+    public void mergeSort(int data[]) {
+        sort(data, 0, data.length - 1);
+
+    }
+
+    // method merge
+    public void merge(int data[], int left, int middle, int right) {
+        int[] temp = new int[data.length];
+        for (int i = left; i <= right; i++) {
+            temp[i] = data[i];
+        }
+        int a = left;
+        int b = middle + 1;
+        int c = left;
+        while (a <= middle && b <= right) {
+            if (temp[a] <= temp[b]) {
+                data[c] = temp[a];
+                a++;
+            } else {
+                data[c] = temp[b];
+                b++;
+            }
+            c++;
+        }
+        int s = middle - a;
+        for (int i = 0; i <= s; i++) {
+            data[c + i] = temp[a + i];
+        }
+    }
+
+    //method sort
+    public void sort(int data[], int left, int right) {
+        if (left < right) {
+            int middle = (left + right) / 2;
+            sort(data, left, middle);
+            sort(data, middle + 1, right);
+            merge(data, left, middle, right);
+        }
+    }
+    //print
+    public void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
 }
