f3df1203ad2468c9bd7385a698b0752084f64b81
Noklent <noklen350@gmail.com>
2024-05-31 22:33:57 +0700

    percobaan 2

--- Minggu 14/Percobaan2/BinaryTreeArray20.java
+++ Minggu 14/Percobaan2/BinaryTreeArray20.java
@@ -1,5 +1,29 @@
 package Percobaan2;
 
 public class BinaryTreeArray20 {
-    
+    int data [];
+    int idxlast;
+
+    public BinaryTreeArray20(int data[]) {
+        this.data = data;
+        idxlast = data.length - 1;
+    }
+    public BinaryTreeArray20(){
+        data = new int[10];
+    }
+
+    void populateData(int data[], int idxLast){
+        this.data = data;
+        this.idxlast = idxLast;
+    }
+    void traverseInOrder(int idxStart){
+        if(idxStart <= idxlast){
+           
+            traverseInOrder(2*idxStart+1);
+           
+            System.out.print(data[idxStart] + " ");
+            traverseInOrder(2*idxStart+2);
+
+        }
+    }
 }
